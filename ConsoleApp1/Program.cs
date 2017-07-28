579b36b0954b697b4eaa8af132ede2b934425072
Ryan Duan <duanpanda@gmail.com>
2017-07-28 15:26:29 -0400

    CapitalizeString(string)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CapitalizeString("samuel"));
+            Console.WriteLine(CapitalizeString("julia"));
+            Console.WriteLine(CapitalizeString("john smith"));
+        }
+
+        static string CapitalizeString(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
