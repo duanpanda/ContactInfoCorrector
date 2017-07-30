3abe8d6e9e052e2730527be9f55f2338486fc9f3
Ryan Duan <duanpanda@gmail.com>
2017-07-29 22:52:26 -0400

    Add rules: CapitalizeStreet, CityProvinceCountryCorrector.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,47 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string inString = "test test2 test test2 test test2";
-            Console.WriteLine(CapitalizeWord(inString));
-
             Abbreviator ae = new Abbreviator();
             ae.Add("Ontario", "ON");
             Console.WriteLine(ae.Abbrev("Ontario"));
-
-            CityProvinceCountryCorrector cc = new CityProvinceCountryCorrector();
-            if (cc.LoadCityTable())
-            {
-                string[] record = { "Toronto", "Nova Scotia", "China" };
-                string[] result = cc.CorrectCityAddress(record);
-                Console.WriteLine(record[0] + ", " + record[1] + ", " + record[2] + " ==> " +
-                    result[0] + ", " + result[1] + ", " + result[2]);
-            }
 
             Console.WriteLine("------------------------------------------------------");
             List<ContactRecord> contacts = new List<ContactRecord>();
              contacts.Add(new ContactRecord("Craig", "KielBUrger", "233 carlton St.",
                 "TORONTO", "Ontario", "M3K 1L3", "USA"));
             contacts[0].print();
+
             ContactInfoCorrector corrector = new ContactInfoCorrector();
             corrector.AddRule(new CapitalizeName());
+            corrector.AddRule(new CapitalizeStreet());
+            CityProvinceCountryCorrector cc = new CityProvinceCountryCorrector();
+            cc.LoadCityTable();
+            corrector.AddRule(cc);
             corrector.ApplyRules(contacts[0]);
-            contacts[0].Street = CapitalizeWord(contacts[0].Street);
-            string[] o = new string[3];
-            o[0] = contacts[0].City;
-            o[1] = contacts[0].Province;
-            o[2] = contacts[0].Country;
-            string[] r = cc.CorrectCityAddress(o);
-            contacts[0].City = r[0];
-            contacts[0].Province = r[1];
-            contacts[0].Country = r[2];
-            contacts[0].print();
-        }
 
-        static string CapitalizeWord(string s)
-        {
-            TextInfo cultInfo = new CultureInfo("en-US", false).TextInfo;
-            string output = cultInfo.ToTitleCase(s);
-            return output;
+            contacts[0].print();
         }
     }
 
@@ -79,39 +57,6 @@ namespace ConsoleApp1
             {
                 return word;
             }
-        }
-    }
-
-    class CityProvinceCountryCorrector
-    {
-        private List<string[]> cityTable;
-
-        public Boolean LoadCityTable()
-        {
-            cityTable = new List<string[]>();
-
-            string[] city = new string[3];
-            city[0] = "Toronto";
-            city[1] = "ON";
-            city[2] = "Canada";
-
-            cityTable.Add(city);
-            return true;
-        }
-
-        // return 
-        public string[] CorrectCityAddress(string[] cityProvinceCountry)
-        {
-            for (int i = 0; i < cityTable.Count; ++i)
-            {
-                // string comparison (ignore case)
-                if (String.Compare(cityTable[i][0], cityProvinceCountry[0], true) == 0)
-                {
-                    return cityTable[i];
-                }
-            }
-            // not found, return the original
-            return cityProvinceCountry;
         }
     }
 
@@ -220,6 +165,65 @@ namespace ConsoleApp1
                 return string.Empty;
             }
             return char.ToUpper(s[0]) + s.Substring(1).ToLower();
+        }
+    }
+
+    class CapitalizeStreet : CorrectorRule
+    {
+        public override void ApplyOn(ContactRecord contact)
+        {
+            contact.Street = CapitalizeWord(contact.Street);
+        }
+
+        static string CapitalizeWord(string s)
+        {
+            TextInfo cultInfo = new CultureInfo("en-US", false).TextInfo;
+            string output = cultInfo.ToTitleCase(s);
+            return output;
+        }
+    }
+
+    class CityProvinceCountryCorrector : CorrectorRule
+    {
+        private List<string[]> cityTable;
+
+        public override void ApplyOn(ContactRecord contact)
+        {
+            string[] o = new string[3];
+            o[0] = contact.City;
+            o[1] = contact.Province;
+            o[2] = contact.Country;
+            string[] r = CorrectCityAddress(o);
+            contact.City = r[0];
+            contact.Province = r[1];
+            contact.Country = r[2];
+        }
+
+        public Boolean LoadCityTable()
+        {
+            cityTable = new List<string[]>();
+
+            string[] city = new string[3];
+            city[0] = "Toronto";
+            city[1] = "ON";
+            city[2] = "Canada";
+
+            cityTable.Add(city);
+            return true;
+        }
+
+        private string[] CorrectCityAddress(string[] cityProvinceCountry)
+        {
+            for (int i = 0; i < cityTable.Count; ++i)
+            {
+                // string comparison (ignore case)
+                if (String.Compare(cityTable[i][0], cityProvinceCountry[0], true) == 0)
+                {
+                    return cityTable[i];
+                }
+            }
+            // not found, return the original
+            return cityProvinceCountry;
         }
     }
 }
